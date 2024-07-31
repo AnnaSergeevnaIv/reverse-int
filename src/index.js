4bddef3d2c56d8a9844f06313d3e2309f61c7ae1
AnnaSergeevnaIv <kara4eva.anya@gmail.com>
2024-07-31 10:42:20 +0300

    feat: add tast solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let i = Math.abs(n)
+    let array = []
+  while (i >= 1) {
+        array.unshift(i % 10)
+        i = Math.floor(i / 10)
+  }
+  array = array.reverse()
+  return +array.join('')
 }
